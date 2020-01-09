7d5093aa9787efeb1e3d5d0e6e6e889c4811f684
Hamza Belmellouki <hamzabelmellouki123@gmail.com>
2020-01-09 10:45:51 +0100

    formating

--- scraping/src/main/java/DbConnection.java
+++ scraping/src/main/java/DbConnection.java
@@ -1,19 +1,17 @@
-import javax.swing.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DbConnection {
-        Connection conn = null;
-        public static Connection dbConnector()  {
+    Connection conn = null;
 
-            try {
-                //Class.forName("com.mysql.cj.jdbc.Driver");
-                Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/hamzabelmellouki","hamzabelmellouki","");
-                return conn;
-            }
-            catch(Exception e) {
-                e.printStackTrace();
-                return null;
-            }
+    public static Connection dbConnector() {
+
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/hamzabelmellouki", "hamzabelmellouki", "");
+            return conn;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
+    }
 }
